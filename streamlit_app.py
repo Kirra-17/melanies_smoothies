eecb499bb2b6cbab445a1003cbd0f6603158670c
Anass <ansaoudi@gmail.com>
2026-08-25 15:37:47 +0200

    Update streamlit_app.py
    Testing pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,11 @@ name_on_smoothie = st.text_input("Name on Smoothie: ")
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width = True)
+# st.dataframe(data=my_dataframe, use_container_width = True)
+# st.stop()
+
+pd_dt=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
